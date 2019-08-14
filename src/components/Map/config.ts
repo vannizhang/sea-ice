7d6df74e2eafad818484408f712b90f991ed17a2
Jinnan Zhang <vannizhang@gmail.com>
2019-08-14 14:41:35 -0700

    add sea ice ext layers to map

--- src/components/Map/config.ts
+++ src/components/Map/config.ts
@@ -3,6 +3,36 @@ const MapConfig = {
     web_map_id: {
         antarctic: '8bb127da2b0a4d1290d83072cbc8548b', //'9abb66054ee94c5c9e066dde3bf8aef8',
         arctic: 'beec7fd80cb24356a5f05407976c6696'
+    },
+    sea_ice_ext_feature_service: {
+        url: {
+            antarctic: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/seaice_extent_S_v1/FeatureServer/0',
+            arctic: 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/seaice_extent_N_v1/FeatureServer/0'
+        },
+        fields: {
+            date: 'Rec_Date',
+            year: 'Rec_Year',
+            month: 'Rec_Month',
+        },
+        style: {
+            fillColor: 'rgba(89,255,252,.3)',
+            outlineColor: 'rgba(89,255,252,1)'
+        }
+    },
+    median_sea_ice_ext_feature_service: {
+        url: {
+            antarctic: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/Median_Sea_Ice_Extent_for_the_Antarctic/FeatureServer/0',
+            arctic: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/Median_Sea_Ice_Extent_for_the_Arctic/FeatureServer/0'
+        },
+        fields: {
+            date: 'Rec_Date',
+            year: 'Rec_Year',
+            month: 'Rec_Month',
+        },
+        style: {
+            fillColor: 'rgba(0,0,0,0)',
+            outlineColor: '#fff766'
+        }
     }
 };
 
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -8,6 +8,10 @@ import { MapConfig } from './config';
 import { PolarRegion } from '../../types';
 import IMapView from 'esri/views/MapView';
 import IWebMap from "esri/WebMap";
+import IFeatureLayer from "esri/layers/FeatureLayer";
+import ISimpleRenderer from "esri/renderers/SimpleRenderer";
+import ISimpleFillSymbol from "esri/symbols/SimpleFillSymbol";
+import ISimpleLineSymbol from "esri/symbols/SimpleLineSymbol";
 
 interface IProps {
     polarRegion:PolarRegion,
@@ -72,13 +76,70 @@ export default class Map extends React.PureComponent<IProps, IState> {
             'esri/WebMap'
         ]) as Promise<Modules>);
 
+        const seaIceExtLayers = await this.getSeaIceExtLayers();
+
         const webmap = new WebMap({
             portalItem: {
                 id: MapConfig.web_map_id[this.props.polarRegion]
-            }
+            },
+            layers:[
+                ...seaIceExtLayers
+            ]
         });
 
         return webmap;
+    }
+
+    async getSeaIceExtLayers(){
+
+        type Modules = [
+            typeof IFeatureLayer,
+            typeof ISimpleRenderer,
+            typeof ISimpleFillSymbol,
+            typeof ISimpleLineSymbol
+        ];
+
+        const [ FeatureLayer, SimpleRenderer, SimpleFillSymbol, SimpleLineSymbol ] = await (loadModules([
+            'esri/layers/FeatureLayer',
+            "esri/renderers/SimpleRenderer",
+            "esri/symbols/SimpleFillSymbol",
+            "esri/symbols/SimpleLineSymbol"
+        ]) as Promise<Modules>);
+
+        const rendererSeaIceExt = new SimpleRenderer({ 
+            symbol: new SimpleFillSymbol({
+                color: MapConfig.sea_ice_ext_feature_service.style.fillColor,
+                outline: {  // autocasts as new SimpleLineSymbol()
+                    width: 1,
+                    color:  MapConfig.sea_ice_ext_feature_service.style.outlineColor
+                }
+            })
+        });
+
+        const seaIceExtLayer = new FeatureLayer({
+            url: MapConfig.sea_ice_ext_feature_service.url[this.props.polarRegion],
+            definitionExpression: 'Rec_Year = 2015 AND Rec_Month = 12',
+            visible: true,
+            renderer: rendererSeaIceExt
+        });
+
+        const rendererMedianSeaIceExt = new SimpleRenderer({ 
+            symbol: new SimpleLineSymbol({
+                color: MapConfig.median_sea_ice_ext_feature_service.style.outlineColor,
+                width: 1,
+                style: 'dash'
+            })
+        });
+
+        const medianSeaIceExtLayer = new FeatureLayer({
+            url: MapConfig.median_sea_ice_ext_feature_service.url[this.props.polarRegion],
+            definitionExpression: 'Rec_Year = 2015 AND Rec_Month = 12',
+            visible: true,
+            renderer: rendererMedianSeaIceExt
+        });
+
+        return [medianSeaIceExtLayer, seaIceExtLayer];
+
     }
 
     mapViewOnReadyHandler(mapView:IMapView){
